1216bb3d2ba9401771cede3d1508047079d12805
prosif <prosif@gmail.com>
2015-10-11 21:14:33 -0700

    actually working this time

--- generate.py
+++ generate.py
@@ -2,22 +2,31 @@ import random
 import re
 
 def main():
-    words = {}
+    words = []
+    longest = 0
     with open("temp.txt") as f:
         for line in f:
             word_list = re.sub("[^\S]", " ", line[:line.index('[')]).split()
             i = 0
             while i < len(word_list):
-                if word_list[i] not in words:
-                    words[word_list[i]] = []
-                if i not in words[word_list[i]]:
-                    words[word_list[i]].append(i)
+                if i not in words:
+                    words.append([])
+                if word_list[i] not in words[i]:
+                    words[i].append(word_list[i])
                 i += 1
+            if(i > longest):
+                longest = i
 
+    
+    length = random.randint(1, longest)
+    count = 0
     to_return = ""
-    for word in words:    
-        index = random.randint(0, len(words[word])
-        to_return += word[
+    while count < length:
+        rando = random.randint(0, len(words[count]) - 1)
+        to_return += str(words[count][rando])
+        to_return += " "
+        count += 1
+    print to_return
 
 if __name__ == '__main__':
     main()
